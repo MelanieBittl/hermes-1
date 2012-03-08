eedf90ec6d3f9119b77959003f31aebca27d2056
Melanie Bittl <melanie.bittl@am.uni-erlangen.de>
2012-03-08 13:01:47 +0100

    FCT adde in text_examples

---
+++ hermes2d/test_examples/FCT_example/FCT_hp_adap/definitions.h
@@ -0,0 +1,260 @@
+#ifndef __DEFINITIONS_H
+#define __DEFINITIONS_H
+
+#include "hermes2d.h"
+ #define PI (3.141592653589793)   
+
+
+
+using namespace Hermes;
+using namespace Hermes::Hermes2D;
+
+
+//---------------Massematrix-----------
+
+class CustomMatrixFormVolMassmatrix : public MatrixFormVol<double>   
+
+{
+  public:
+    // This weak form is custom since it contains a nonlinearity in the diffusion term.
+    CustomMatrixFormVolMassmatrix(int i, int j, double time_step) 
+      : MatrixFormVol<double>(i, j, HERMES_ANY, HERMES_NONSYM), time_step(time_step) { };
+
+    template<typename Real, typename Scalar>
+    Scalar matrix_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, 
+                       Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const;
+
+    virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *u, 
+                 Func<double> *v, Geom<double> *e, ExtData<double> *ext) const;
+
+    virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, Func<Ord> *v, 
+            Geom<Ord> *e, ExtData<Ord> *ext) const;
+    
+    // Members.  
+    double time_step;
+};
+
+
+class VectorFormVolMass : public VectorFormVol<double>
+{
+public:
+	VectorFormVolMass(int i, double  time_step) : VectorFormVol<double>(i), time_step(time_step) { };
+
+	template<typename Real, typename Scalar>
+	Scalar vector_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const;
+
+	virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *v, Geom<double> *e, ExtData<double> *ext) const;
+
+	virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext) const;
+
+	// Members.  
+	double time_step;
+
+};
+
+class  CustomWeakFormMassmatrix  : public WeakForm<double>     
+{
+public:
+  CustomWeakFormMassmatrix(double time_step,Solution<double>* sln_prev_time);
+	~CustomWeakFormMassmatrix();
+};
+
+//---------------Konvektion-----------
+class CustomMatrixFormVolConvection : public MatrixFormVol<double>   
+{
+public:
+  
+  CustomMatrixFormVolConvection(int i, int j) 
+    : MatrixFormVol<double>(i, j, HERMES_ANY, HERMES_NONSYM) { }
+
+  template<typename Real, typename Scalar>
+  Scalar matrix_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, 
+                     Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const;
+
+  virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *u, 
+               Func<double> *v, Geom<double> *e, ExtData<double> *ext) const;
+
+  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, Func<Ord> *v, 
+          Geom<Ord> *e, ExtData<Ord> *ext) const;  
+
+};
+
+
+class VectorFormVolConvection : public VectorFormVol<double>
+{
+public:
+  VectorFormVolConvection(int i) : VectorFormVol<double>(i){ }
+
+  template<typename Real, typename Scalar>
+  Scalar vector_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const;
+
+  virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *v, Geom<double> *e, ExtData<double> *ext) const;
+
+  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext) const;
+
+};
+
+
+class CustomWeakFormConvection : public WeakForm<double>    //Konvektion
+{
+public:
+  CustomWeakFormConvection(Solution<double>* sln_prev_time);
+	~CustomWeakFormConvection();  
+};
+
+
+
+
+
+
+//-----------------Residual for error estimator--------------------------------
+class ResidualMatForm : public MatrixFormVol<double>
+{
+public:
+  ResidualMatForm(int i, int j, double tau) 
+    : MatrixFormVol<double>(i, j, HERMES_ANY, HERMES_NONSYM), tau(tau) { }
+
+  template<typename Real, typename Scalar>
+  Scalar matrix_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, 
+                     Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const;
+  
+
+  virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *u, 
+               Func<double> *v, Geom<double> *e, ExtData<double> *ext) const;
+
+  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, Func<Ord> *v, 
+          Geom<Ord> *e, ExtData<Ord> *ext) const;    
+  // Members.  
+  double tau;
+};
+
+
+class VectorResidual : public VectorFormVol<double>
+{
+public:
+  VectorResidual(int i,double tau) : VectorFormVol<double>(i), tau(tau) { }
+
+  template<typename Real, typename Scalar>
+  Scalar vector_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const ;
+ 
+
+  virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *v, Geom<double> *e, ExtData<double> *ext) const;
+
+  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext) const;
+
+  // Members.    
+  double tau;
+};
+class ResidualForm : public WeakForm<double>
+{
+public:
+  ResidualForm( double tau, Solution<double>* sln_prev_time, Solution<double>* ref);
+	~ResidualForm();
+};
+
+//---------Gradient Reconstruction---------------
+
+class GradientReconstructionMatForm_1 : public MatrixFormVol<double>
+{
+public:
+  GradientReconstructionMatForm_1(int i, int j) 
+    : MatrixFormVol<double>(i, j, HERMES_ANY, HERMES_NONSYM){ }
+
+  template<typename Real, typename Scalar>
+  Scalar matrix_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, 
+                     Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const;
+  
+
+  virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *u, 
+               Func<double> *v, Geom<double> *e, ExtData<double> *ext) const;
+
+  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, Func<Ord> *v, 
+          Geom<Ord> *e, ExtData<Ord> *ext) const;    
+
+};
+
+
+class GradientReconstructionVectorForm_1 : public VectorFormVol<double>
+{
+public:
+  GradientReconstructionVectorForm_1(int i) : VectorFormVol<double>(i) { }
+
+  template<typename Real, typename Scalar>
+  Scalar vector_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const ;
+ 
+
+  virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *v, Geom<double> *e, ExtData<double> *ext) const;
+
+  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext) const;
+
+
+};
+class GradientReconstruction_1 : public WeakForm<double>
+{
+public:
+  GradientReconstruction_1(Solution<double>* sln);
+	~GradientReconstruction_1();
+};
+
+
+class GradientReconstructionMatForm_2 : public MatrixFormVol<double>
+{
+public:
+  GradientReconstructionMatForm_2(int i, int j) 
+    : MatrixFormVol<double>(i, j, HERMES_ANY, HERMES_NONSYM){ }
+
+  template<typename Real, typename Scalar>
+  Scalar matrix_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, 
+                     Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const;
+  
+
+  virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *u, 
+               Func<double> *v, Geom<double> *e, ExtData<double> *ext) const;
+
+  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, Func<Ord> *v, 
+          Geom<Ord> *e, ExtData<Ord> *ext) const;    
+
+};
+
+
+class GradientReconstructionVectorForm_2 : public VectorFormVol<double>
+{
+public:
+  GradientReconstructionVectorForm_2(int i) : VectorFormVol<double>(i) { }
+
+  template<typename Real, typename Scalar>
+  Scalar vector_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext) const ;
+ 
+
+  virtual double value(int n, double *wt, Func<double> *u_ext[], Func<double> *v, Geom<double> *e, ExtData<double> *ext) const;
+
+  virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext) const;
+
+
+};
+class GradientReconstruction_2 : public WeakForm<double>
+{
+public:
+  GradientReconstruction_2(Solution<double>* sln);
+	~GradientReconstruction_2();
+};
+
+//------------------- Initial condition ----------------
+
+class CustomInitialCondition : public ExactSolutionScalar<double>
+{
+public:
+  CustomInitialCondition(Mesh* mesh) : ExactSolutionScalar<double>(mesh) {};
+   ~CustomInitialCondition(){};
+
+  virtual void derivatives (double x, double y, double& dx, double& dy) const ;
+
+  virtual double value (double x, double y) const;
+
+ virtual Ord ord(Ord x, Ord y) const ;
+};
+
+
+
+#endif
+
---
+++ hermes2d/test_examples/FCT_example/FCT_hp_adap/lumped_projection.h
@@ -0,0 +1,117 @@
+#ifndef __LUMPED_PROJECTION_H
+#define __LUMPED_PROJECTION_H
+
+
+#include "../function/solution.h"
+#include "../forms.h"
+#include "../weakform/weakform.h"
+
+using namespace Hermes;
+using namespace Hermes::Hermes2D;
+
+
+
+
+//template<typename double>
+class Lumped_Projection
+{
+public:
+  static void project_lumped( const Space<double>* space, MeshFunction<double>* source_meshfn,
+                             double* target_vec, MatrixSolverType matrix_solver = SOLVER_UMFPACK,UMFPackMatrix<double>*  mat  = NULL);
+
+
+protected:
+  static void project_internal( const Space<double>* space, WeakForm<double>* wf, double* target_vec,
+                               MatrixSolverType matrix_solver, UMFPackMatrix<double>*  mat = NULL);
+
+
+
+  // Jacobian matrix (same as stiffness matrix since projections are linear).
+  class ProjectionLumpedMatrixFormVol : public MatrixFormVol<double>
+  {
+  public:
+    ProjectionLumpedMatrixFormVol(int i, int j) : MatrixFormVol<double>(i, j)
+    {
+      //this->adapt_eval = false;      
+    }
+
+    double value(int n, double *wt, Func<double> *u_ext[], Func<double> *u, Func<double> *v,
+                 Geom<double> *e, ExtData<double> *ext) const
+    {      
+      
+        return lumped_projection_biform<double, double>(n, wt, u_ext, u, v, e, ext);
+     
+    }
+
+    Hermes::Ord ord(int n, double *wt, Func<Hermes::Ord> *u_ext[], Func<Hermes::Ord> *u, Func<Hermes::Ord> *v,
+          Geom<Hermes::Ord> *e, ExtData<Hermes::Ord> *ext) const
+    {
+          
+        return lumped_projection_biform<Hermes::Ord, Hermes::Ord>(n, wt, u_ext, u, v, e, ext);
+     
+    }
+
+  private:
+  
+
+		template<typename TestFunctionDomain, typename SolFunctionDomain>
+        static SolFunctionDomain lumped_projection_biform(int n, double *wt, Func<SolFunctionDomain> *u_ext[], Func<TestFunctionDomain> *u,
+          Func<TestFunctionDomain> *v, Geom<TestFunctionDomain> *e, ExtData<SolFunctionDomain> *ext) 
+        {
+          _F_
+            SolFunctionDomain result = SolFunctionDomain(0);
+          for (int i = 0; i < n; i++)
+            result += wt[i] * (u->val[i] * v->val[i]);
+          return result;
+        }
+
+  };
+
+  // Residual.
+  class ProjectionLumpedVectorFormVol : public VectorFormVol<double>
+  {
+  public:
+    ProjectionLumpedVectorFormVol(int i, MeshFunction<double>* ext) : VectorFormVol<double>(i)
+    {
+      //this->adapt_eval = false;     
+      this->ext = Hermes::vector<MeshFunction<double>*>();
+      this->ext.push_back(ext);
+    }
+
+    double value(int n, double *wt, Func<double> *u_ext[], Func<double> *v,
+                 Geom<double> *e, ExtData<double> *ext) const
+    {
+     
+      
+        return lumped_projection_residual<double, double>(n, wt, u_ext, v, e, ext);
+     
+    }
+
+    Hermes::Ord ord(int n, double *wt, Func<Hermes::Ord> *u_ext[], Func<Hermes::Ord> *v,
+          Geom<Hermes::Ord> *e, ExtData<Hermes::Ord> *ext) const
+    {
+      
+        return lumped_projection_residual<Hermes::Ord, Hermes::Ord>(n, wt, u_ext, v, e, ext);
+    
+    }
+
+  private:
+
+   template<typename TestFunctionDomain, typename SolFunctionDomain>
+        SolFunctionDomain lumped_projection_residual(int n, double *wt, Func<SolFunctionDomain> *u_ext[], Func<TestFunctionDomain> *v,
+          Geom<TestFunctionDomain> *e, ExtData<SolFunctionDomain> *ext) const
+        {
+          _F_
+            SolFunctionDomain result = SolFunctionDomain(0);
+          for (int i = 0; i < n; i++)
+							result += wt[i] * (ext->fn[0]->val[i]) * v->val[i];
+            //result += wt[i] * (u_ext[this->i]->val[i] - ext->fn[0]->val[i]) * v->val[i];
+							
+          return result;
+        }
+   
+  };
+};
+
+
+#endif
