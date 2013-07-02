4f48d278dcd787f4e1d84928509f4f430a819a38
Lukas Korous <lukas.korous@gmail.com>
2013-07-02 17:50:27 +0200

    Testing of DG limiting.

--- hermes2d/test_examples/10-linear-advection-dg-adapt/main.cpp
+++ hermes2d/test_examples/10-linear-advection-dg-adapt/main.cpp
@@ -1,5 +1,3 @@
-#define HERMES_REPORT_ALL
-#define HERMES_REPORT_FILE "application.log"
 #include "definitions.h"
 
 //  This example solves a linear advection equation using Dicontinuous Galerkin (DG) method.
@@ -15,7 +13,7 @@
 //  The following parameters can be changed:
 
 // Number of initial uniform mesh refinements.
-const int INIT_REF = 2;
+const int INIT_REF = 3;
 // Initial polynomial degrees of mesh elements in vertical and horizontal directions.
 const int P_INIT = 1;
 // This is a quantitative parameter of the adapt(...) function and
@@ -31,7 +29,7 @@ Adapt<double> adaptivity(&errorCalculator, &stoppingCriterion);
 // Predefined list of element refinement candidates.
 const CandList CAND_LIST = H2D_H_ANISO;
 // Stopping criterion for adaptivity.
-const double ERR_STOP = 1e-1;
+const double ERR_STOP = 1e11;
 
 int main(int argc, char* args[])
 {
@@ -44,33 +42,28 @@ int main(int argc, char* args[])
   for (int i=0; i<INIT_REF; i++)
     mesh->refine_all_elements();
 
-  // Create an L2 space->
-  SpaceSharedPtr<double> space(new L2Space<double>(mesh, P_INIT, new L2ShapesetTaylor));
+  // Create an L2 space.
+  SpaceSharedPtr<double> coarse_space(new L2Space<double>(mesh, 0, new L2ShapesetTaylor));
+  SpaceSharedPtr<double> fine_space(new L2Space<double>(mesh, P_INIT, new L2ShapesetTaylor));
 
   // Initialize refinement selector.
   L2ProjBasedSelector<double> selector(CAND_LIST);
   selector.set_error_weights(1.,1.,1.);
 
-  // DOF and CPU convergence graphs.
-  SimpleGraph graph_dof_est, graph_cpu_est;
-
-  // Display the mesh->
-  OrderView oview("Coarse mesh", new WinGeom(0, 0, 440, 350));
-  oview.show(space);
-
   MeshFunctionSharedPtr<double> sln(new Solution<double>);
-  MeshFunctionSharedPtr<double> ref_sln(new Solution<double>);
+  MeshFunctionSharedPtr<double> ref_coarse_sln(new Solution<double>);
+  MeshFunctionSharedPtr<double> ref_fine_sln(new Solution<double>);
 
   // Initialize the weak formulation.
   CustomWeakForm wf("Bdy_bottom_left", mesh);
-
   ScalarView view1("Solution", new WinGeom(900, 0, 450, 350));
   view1.fix_scale_width(60);
 
   // Initialize linear solver.
-  Hermes::Hermes2D::LinearSolver<double> linear_solver(&wf, space);
+  Hermes::Hermes2D::LinearSolver<double> linear_solver;
+  linear_solver.set_weak_formulation(&wf);
 
-  adaptivity.set_space(space);
+  adaptivity.set_space(fine_space);
 
   int as = 1; bool done = false;
   do
@@ -79,38 +72,94 @@ int main(int argc, char* args[])
     // and setup reference space->
     Mesh::ReferenceMeshCreator ref_mesh_creator(mesh);
     MeshSharedPtr ref_mesh = ref_mesh_creator.create_ref_mesh();
-    Space<double>::ReferenceSpaceCreator ref_space_creator(space, ref_mesh, 0);
-    SpaceSharedPtr<double> ref_space = ref_space_creator.create_ref_space();
 
-    linear_solver.set_space(ref_space);
+    Space<double>::ReferenceSpaceCreator ref_coarse_space_creator(coarse_space, ref_mesh, 0);
+    SpaceSharedPtr<double> ref_coarse_space = ref_coarse_space_creator.create_ref_space();
+
+    Space<double>::ReferenceSpaceCreator ref_fine_space_creator(fine_space, ref_mesh, 0);
+    SpaceSharedPtr<double> ref_fine_space = ref_fine_space_creator.create_ref_space();
 
     // Solve the linear system. If successful, obtain the solution.
+#pragma region old_implementation
+    /*
     try
     {
+      linear_solver.set_space(ref_coarse_space);
       linear_solver.solve();
-      Solution<double>::vector_to_solution(linear_solver.get_sln_vector(), ref_space, ref_sln);
+      Solution<double>::vector_to_solution(linear_solver.get_sln_vector(), ref_coarse_space, ref_coarse_sln);
+
+      view1.show(ref_coarse_sln);
+      view1.set_title("Coarse solution");
+      view1.wait_for_keypress();
+
+      linear_solver.set_space(ref_fine_space);
+      linear_solver.solve();
+
+      Solution<double>::vector_to_solution(linear_solver.get_sln_vector(), ref_fine_space, ref_fine_sln);
+      view1.show(ref_fine_sln);
+      view1.set_title("Unlimited fine solution");
+      view1.wait_for_keypress();
+
+      FluxLimiter* flux_limiter = new FluxLimiter(FluxLimiter::Kuzmin, linear_solver.get_sln_vector(), ref_fine_space, true);
+      flux_limiter->limit_according_to_detector();
+      flux_limiter->get_limited_solution(ref_fine_sln);
+
+      view1.show(ref_fine_sln);
+      view1.set_title("Limited fine solution - UNseparated coarse / fine component");
+      view1.wait_for_keypress();
+
+      Element* e;
+      for_all_active_elements(e, ref_fine_sln->get_mesh())
+      {
+        AsmList<double> al;
+        ref_fine_space->get_element_assembly_list(e, &al);
+        for(unsigned int shape_i = 0; shape_i < al.get_cnt(); shape_i++)
+          if(ref_fine_space->get_shapeset()->get_order(al.get_idx()[shape_i], e->get_mode()) == 0)
+            linear_solver.get_sln_vector()[al.get_dof()[shape_i]] = 0.0;
+      }
+
+      double* coarse_sln_vector = new double[ref_fine_space->get_num_dofs()];
+      OGProjection<double>::project_global(ref_fine_space, ref_coarse_sln, coarse_sln_vector);
+      for(int i = 0; i < ref_fine_space->get_num_dofs(); i++)
+        coarse_sln_vector[i] += linear_solver.get_sln_vector()[i];
+
+      Solution<double>::vector_to_solution(coarse_sln_vector, ref_fine_space, ref_fine_sln);
+
+      view1.set_title("Limited fine solution - separated coarse / fine component");
+      view1.show(ref_fine_sln);
+      OGProjection<double>::project_global(fine_space, ref_fine_sln, sln, HERMES_L2_NORM);
     }
     catch(std::exception& e)
     {
       std::cout << e.what();
     }
-    // Project the fine mesh solution onto the coarse mesh.
-    OGProjection<double> ogProjection;
-    ogProjection.project_global(space, ref_sln, sln, HERMES_L2_NORM);
+    */
+#pragma endregion
 
-    // View the coarse mesh solution.
-    view1.show(ref_sln);
-    oview.show(space);
+#pragma region new_implementation
+    try
+    {
+      linear_solver.set_space(ref_fine_space);
+      linear_solver.solve();
+
+      PostProcessing::VertexBasedLimiter limiter(ref_fine_space, linear_solver.get_sln_vector());
+      ref_fine_sln = limiter.get_solution();
+
+      // Solution<double>::vector_to_solution(linear_solver.get_sln_vector(), ref_fine_space, ref_fine_sln);
+      view1.show(ref_fine_sln);
+      OGProjection<double>::project_global(fine_space, ref_fine_sln, sln, HERMES_L2_NORM);
+    }
+    catch(std::exception& e)
+    {
+      std::cout << e.what();
+    }
+#pragma endregion
 
     // Calculate element errors and total error estimate.
-    errorCalculator.calculate_errors(sln, ref_sln);
+    errorCalculator.calculate_errors(sln, ref_fine_sln);
     double err_est_rel = errorCalculator.get_total_error_squared() * 100;
 
     std::cout << "Error: " << err_est_rel << "%." << std::endl;
-
-    // Add entry to DOF and CPU convergence graphs.
-    graph_dof_est.add_values(Space<double>::get_num_dofs(space), err_est_rel);
-    graph_dof_est.save("conv_dof_est.dat");
 
     // If err_est_rel too large, adapt the mesh->
     if(err_est_rel < ERR_STOP)
